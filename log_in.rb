771765df44a9d3ac21a67a48d5d1d9b51ac54a23
universtounding <kendmorse@gmail.com>
2018-11-29 18:10:01 -0500

    create new user start

--- log_in.rb
+++ log_in.rb
@@ -1,4 +1,32 @@
  class NewUser
+   attr_accessor :user, :email, :password
 
+  def initialize
+    @user = user
+    @email = email
+    @password = password
+  end
 
- end
+  def user_name
+     puts "Enter your username here:"
+     user = gets.chomp
+     @user = user
+  end
+
+  def email
+    puts "What is your email?"
+    email = gets.chomp
+    @email = email
+  end
+
+  def password
+    puts "What is your password?"
+    password = gets.chomp
+    @password = password
+  end
+  end
+
+usr_name = NewUser.new
+usr_name.email
+usr_name.user_name
+usr_name.password
